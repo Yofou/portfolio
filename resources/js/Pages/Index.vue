e0b3bd5d9fd86e58bda76b3b467890e5217f4f6d
Yofou <exodias1965@gmail.com>
2022-10-25 07:24:21 +0100

    chore: Init project

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { Head } from '@inertiajs/inertia-vue3';
+
+    defineProps({})
+</script>
+
+<template>
+    <Head title="Yofou | Home" />
+
+    <main>
+        <h1>Welcome</h1>
+    </main>
+</template>
