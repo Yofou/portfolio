54824f299d6342a8b1435396ca152a731e1cc8eb
Yofou <exodias1965@gmail.com>
2022-10-29 07:33:09 +0100

    chore: What I've worked with + minor ui fixes

---
+++ resources/js/Components/UsedBadge.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  src: string,
+  alt?: string,
+}>()
+</script>
+
+<template>
+  <p class="bg-purple-300 rounded-[5px] group grid text-white/80 grid-cols-[20px,max-content] items-center font-bold gap-2 tracking-wider uppercase cursor-pointer px-4 py-2">
+    <img class="pointer-events-none select-none" :src="src" :alt="props.alt ?? ''">
+    <slot />
+  </p>
+</template>
---
+++ resources/js/Components/WhatIveUsed.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+  import UsedBadge from "./UsedBadge.vue"
+</script>
+
+<template>
+  <section class="w-full flex flex-wrap gap-2">
+    <UsedBadge src="/icons/react.svg">React</UsedBadge>
+    <UsedBadge src="/icons/svelte.svg">Svelte</UsedBadge>
+    <UsedBadge src="/icons/vue.svg">Vuejs</UsedBadge>
+    <UsedBadge src="/icons/next.svg">Next</UsedBadge>
+    <UsedBadge src="/icons/nuxt.svg">Nuxt</UsedBadge>
+    <UsedBadge src="/icons/quasar.svg">Quasar</UsedBadge>
+    <UsedBadge src="/icons/gatsby.svg">Gatsby</UsedBadge>
+    <UsedBadge src="/icons/react-query.svg">React Query</UsedBadge>
+    <UsedBadge src="/icons/redux.svg">Redux</UsedBadge>
+    <UsedBadge src="/icons/react-hook-form.svg">React Hook Form</UsedBadge>
+    <UsedBadge src="/icons/p5.svg">p5.js</UsedBadge>
+    <UsedBadge src="/icons/tailwind.svg">TailwindCss</UsedBadge>
+    <UsedBadge src="/icons/windi.svg">WindiCss</UsedBadge>
+    <UsedBadge src="/icons/vite.svg">Vite</UsedBadge>
+    <UsedBadge src="/icons/fasitfy.svg">Fasitfy</UsedBadge>
+    <UsedBadge src="/icons/express.svg">Express.js</UsedBadge>
+    <UsedBadge src="/icons/nest.svg">Nestjs</UsedBadge>
+    <UsedBadge src="/icons/sequelize.svg">Sequelize</UsedBadge>
+    <UsedBadge src="/icons/prisma.svg">Prisma</UsedBadge>
+    <UsedBadge src="/icons/typescript.svg">Typescript</UsedBadge>
+    <UsedBadge src="/icons/deno.svg">Deno js</UsedBadge>
+    <UsedBadge src="/icons/strapi.svg">Strapi</UsedBadge>
+    <UsedBadge src="/icons/docker.svg">Docker</UsedBadge>
+    <UsedBadge src="/icons/vercel.svg">Vercel</UsedBadge>
+    <UsedBadge src="/icons/vultr.svg">Vultr</UsedBadge>
+    <UsedBadge src="/icons/ovh.svg">OVH</UsedBadge>
+    <UsedBadge src="/icons/cloudflare.svg">Cloudflare</UsedBadge>
+    <UsedBadge src="/icons/neovim.svg">Neovim</UsedBadge>
+    <UsedBadge src="/icons/vscode.svg">Visual Studio Code</UsedBadge>
+    <UsedBadge src="/icons/gitpod.svg">Gitpod</UsedBadge>
+  </section>
+</template>
