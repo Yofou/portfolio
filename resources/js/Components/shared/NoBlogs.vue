7756352df48dfce06d835f50de54404a4139459d
Yofou <reach@yofou.dev>
2022-11-23 04:49:56 +0000

    fix: typo from tunned to tune

--- resources/js/Components/shared/NoBlogs.vue
+++ resources/js/Components/shared/NoBlogs.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex flex-col justify-center text-center gap-2">
     <h2 class="text-[1.5rem] text-yellow-300 font-semibold">No Blogs</h2>
-    <p class="text-white/80 font-[1.3rem]">but stay tunned, I got some coming on the way.</p>
+    <p class="text-white/80 font-[1.3rem]">but stay tuned, I got some coming on the way.</p>
   </section>
 </template>
