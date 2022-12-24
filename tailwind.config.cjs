88a9a507e23ca5dd40985dc6812c0541aec0b53e
Nathan <39814328+Yofou@users.noreply.github.com>
2022-12-24 03:44:47 +0000

    chore: Added Home & blogs page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				jost: ["'Jost'", ...defaultTheme.fontFamily.sans],
+				merriweather: ["'Merriweather'", ...defaultTheme.fontFamily.sans]
+			},
+			gridTemplateColumns: {
+				container: 'minmax(0, var(--container, 1250px))'
+			},
+			colors: {
+				yellow: {
+					300: '#E9AA52'
+				},
+				purple: {
+          300: '#302638',
+					600: '#16131d'
+				}
+			}
+		}
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/forms')]
 }
