905e5dfaa0a3392e41f8a02e81ee6f870119133a
Nathan <39814328+Yofou@users.noreply.github.com>
2022-12-26 02:42:30 +0000

    chore: Manual sitemap mode for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,12 +18,21 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
+const site = 'https://www.yofou.dev'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), robotsTxt({
-    sitemap: false
-  })],
-  site: 'https://www.yofou.dev',
-  output: 'server',
-  adapter: vercel()
+	integrations: [
+		svelte(), 
+		tailwind(), 
+		astroImageTools, 
+		sitemap({
+			customPages: [`${site}/`, `${site}/blogs`]
+		}), 
+		robotsTxt({
+    		sitemap: false
+  		}),
+	],
+  	site,
+  	output: 'server',
+  	adapter: vercel()
 });
