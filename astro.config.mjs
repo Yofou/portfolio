b49679039b218e93146b97e47221ea6aeade4ca9
Nathan <39814328+Yofou@users.noreply.github.com>
2022-12-26 02:40:58 +0000

    chore: SSR mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,14 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind(), astroImageTools, sitemap(), robotsTxt({ sitemap: false })],
-  site: 'https://www.yofou.dev'
+  integrations: [svelte(), tailwind(), astroImageTools, sitemap(), robotsTxt({
+    sitemap: false
+  })],
+  site: 'https://www.yofou.dev',
+  output: 'server',
+  adapter: vercel()
 });
