29252eae36991bbcb91ddc1234b7958b6f82eae8
yofou <reach@yofou.dev>
2022-12-28 07:41:38 +0000

    chore: carousel cursor

--- src/components/shared/Cursor.svelte
+++ src/components/shared/Cursor.svelte
@@ -204,6 +204,16 @@
     observer = new MutationObserver(resyncCursor)
     observer.observe(main, { childList: true, subtree: true, attributes: true, characterData: true })
   })
+
+  const CarouselCursorPop = (node: Element, options?: { duration?: number }) => ({
+	duration: options?.duration ?? 100,
+	css: (t: number) => {
+		return `
+			opacity: ${t};
+			transform: translateY(-50%) scaleY(${0.75 - (0.25 * t)});
+		`
+	}
+  })
 </script>
 
 <svelte:window
@@ -222,7 +232,7 @@
 {#if !isPreview}
 	<div class="fixed z-10 top-0 left-0 w-screen h-screen pointer-events-none noise-bg">
 		<div
-			class="cursor rounded-[var(--borderRadius)] border border-[color:var(--borderColor)]"
+			class="cursor relative rounded-[var(--borderRadius)] border border-[color:var(--borderColor)]"
 			style:--x={`${$coords.x?.toPrecision()}px`}
 			style:--y={`${$coords.y}px`}
 			style:--width={`${$morph.width * $scale}px`}
@@ -230,7 +240,12 @@
 			style:--borderRadius={`${$morph.borderRadius}px`}
 			style:--borderColor={borderColor}
 			style:--opacity={$opacity}
-		/>
+		>
+			{#if modes === "carousel"}
+				<svg transition:CarouselCursorPop class="w-6 h-6 text-yellow-300 absolute top-1/2 left-[-28px] -translate-y-1/2" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+				<svg transition:CarouselCursorPop class="w-6 h-6 text-yellow-300 absolute top-1/2 right-[-28px] -translate-y-1/2" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+			{/if}
+		</div>
 	</div>
 {/if}
 
